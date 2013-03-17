49037b409596a25826c04b6a7b9751758a5a9c94
Dincu <dgmarcellab@gmail.com>
2013-03-17 00:12:52 -0430

    Disminution of spheres
    Added script so that every given interval the spheres will lesser in
    size. Need to add a destruction rutine when desire.

--- 1GAM03/Assets/Scripts/SphereActions.cs
+++ 1GAM03/Assets/Scripts/SphereActions.cs
@@ -2,7 +2,14 @@ using UnityEngine;
 using System.Collections;
 
 public class SphereActions : MonoBehaviour {
+	
+	
 	public Color color;
+	public float disminutionStep = 0.10f;
+	public const int ballDisminutionTime = 1;
+	
+	float initialTime = 0;
+	
 	
 	// Use this for initialization
 	void Start () {
@@ -11,10 +18,26 @@ public class SphereActions : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if(initialTime == 0) initialTime = Time.time;
+		float deltaTime ;
+		deltaTime = Time.time - initialTime;
+		Debug.Log (deltaTime);
+		if (deltaTime >= ballDisminutionTime) {
+			
+			this.transform.localScale = new Vector3(this.transform.localScale.x - disminutionStep, 
+				                                    this.transform.localScale.y - disminutionStep, 
+				                                    this.transform.localScale.z - disminutionStep);
+
+			/*	this.transform.localScale.x -= disminutionStep;	
+			this.transform.localScale.y -= disminutionStep;
+			this.transform.localScale.z -= disminutionStep;*/
+			initialTime = 0;
+		};
+		
 	}
 	
 	void OnGUI () {
-		AdvancedLabel.Draw(new Rect(this.transform.localPosition.x, this.transform.localPosition.y, 100, 100),"a", new NewFontSize(10));
+		//Vector3 positionRect = Camera.main.ScreenPointToRay(this.transform.position).direction;
+		//AdvancedLabel.Draw(new Rect(positionRect.x, positionRect.y, 100, 100),"X", new NewFontSize(10));
 	}
 }
