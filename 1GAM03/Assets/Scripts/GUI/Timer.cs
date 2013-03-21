faa63100a54e4c7c292adda29f5733d5ccbdc13d
Vladimir de la Cruz <vladimirdlc@gmail.com>
2013-03-21 14:39:35 -0430

    Added Timer script
    Ascending/Descending seconds clock based on seconds and fractions

---
+++ 1GAM03/Assets/Scripts/GUI/Timer.cs
@@ -0,0 +1,118 @@
+using UnityEngine;
+using System.Collections;
+ 
+public class Timer : MonoBehaviour {
+
+public int seconds;
+public int fraction;
+
+public int secondsGoal;
+public int fractionGoal;
+ 
+protected float currentTime;
+
+[SerializeField]
+protected bool isAscending = true;
+	
+[SerializeField]
+protected bool isCounting = false;
+
+public GUIStyle style;
+public Rect rposition = new Rect(40, 40, 200, 200);
+
+public bool isTimeOver;
+ 
+
+	protected void Start() 
+	{
+        if (string.IsNullOrEmpty(style.name))
+        {
+            style.fontSize = 40;
+            style.fontStyle = FontStyle.BoldAndItalic;
+            resetGUI();
+        }
+
+        fraction = fraction % 10;
+        seconds = seconds % 60;
+        currentTime = seconds + (fraction / 10.0f);
+	}
+ 
+ 
+	protected void Update() 
+	{
+        count();
+
+        if (!isTimeOver && isGoalTimeOver())
+        {
+            timeOverAction();
+            isTimeOver = true;
+        }
+	}
+
+    protected virtual void resetGUI()
+    {
+        style.normal.textColor = Color.gray;
+    }
+
+    protected virtual void timeOverAction()
+    {
+        style.normal.textColor = Color.red;
+    }
+
+    protected void count()
+    {
+        if (isCounting)
+        {
+            currentTime += (isAscending) ? Time.deltaTime : -Time.deltaTime;
+            seconds = (int)(currentTime);
+            fraction = (int)((currentTime * 10) % 10);
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(rposition, string.Format("{0}''{1}", seconds, fraction), style);
+    }
+ 
+	public void startTimer()
+	{
+		resetTime();
+		isCounting = true;
+	}
+ 	
+	public void resumeTimer()
+	{
+		isCounting = true;
+	}
+	
+	public void pauseTimer()
+	{
+		isCounting = false;
+	}
+
+    private bool isGoalTimeOver()
+    {
+        if (isAscending)
+        {
+            return seconds > secondsGoal ||
+                (seconds == secondsGoal && fraction >= fractionGoal);
+        }
+        else
+        {
+            return seconds <= 0 && fraction <= 0;
+        }
+    }
+
+    public void resetTime()
+    {
+        resetGUI();
+        seconds = 0;
+        fraction = 0;
+        currentTime = 0;
+    }
+
+    public void setCountType(bool isAscending)
+    {
+        this.isAscending = isAscending;
+    }
+}
