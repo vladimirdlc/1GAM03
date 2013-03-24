90798d921fec5c14ad3c6034d4fce6fbc21b770f
Vlad <vladimirdlc@gmail.com>
2013-03-24 00:58:28 -0430

    Movement of balls ingame

--- 1GAM03/Assets/Scripts/GameLogic.cs
+++ 1GAM03/Assets/Scripts/GameLogic.cs
@@ -12,21 +12,42 @@ public class GameLogic : MonoBehaviour {
     public static int currentLevel = 1;
     public int screenMargin = 100;
     public GameObject bongBase;
+    public int mask = 1 << 8;
 
-    List<Vector2> points;
+    public static List<Vector2> points;
 
     public float levelTime = 20;
     private float currentTime;
 
+    private static GameLogic instance;
+
+    public static GameLogic Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameObject("GameLogic").AddComponent<GameLogic>();
+            }
+
+            return instance;
+        }
+    }
+
+    public void OnApplicationQuit()
+    {
+        instance = null;
+    }
+
 	// Use this for initialization
 	void Start () {
+        if (instance == null) instance = gameObject.GetComponent<GameLogic>();
         currentTime = 20;
         points = new List<Vector2>();
         int spacex = (Screen.width-screenMargin)/totalCollumms;
         int spacey = (Screen.height - screenMargin)/totalRows;
         int roffsetx = (int)(Screen.width * randomOffsetPercent);
         int roffsety = (int)(Screen.height * randomOffsetPercent);
-
 
         for (int i = 1; i <= totalRows; i++)
             for (int j = 1; j <= totalCollumms; j++)
@@ -47,14 +68,31 @@ public class GameLogic : MonoBehaviour {
             Ray ray = Camera.main.ScreenPointToRay(point);
             RaycastHit hit;
 
-            if (Physics.Raycast(ray, out hit))
+            if (Physics.Raycast(ray, out hit, mask))
             {
                 SphereActions bong = (Instantiate(bongBase, hit.point, transform.rotation) as GameObject).GetComponent<SphereActions>();
                 bong.letter = lettersShuffled[idx].ToString();
+                bong.targetPos = getRandomSpacePoint();
                 idx++;
             }
         }
 	}
+
+    public Vector3 getRandomSpacePoint()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(points[Random.Range(0,points.Count)]);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, mask))
+        {
+            return hit.point;
+        }
+        else
+        {
+            Debug.LogError("Error, there is no hit");
+            return Vector3.zero;
+        }
+    }
 
     void Update()
     {
--- 1GAM03/Assets/Scripts/SphereActions.cs
+++ 1GAM03/Assets/Scripts/SphereActions.cs
@@ -11,14 +11,15 @@ public class SphereActions : MonoBehaviour {
     public string letter;
 	public int offsetx = 6;
 	public int offsety = 20;
-	
+
+    public Vector3 targetPos;
+
 	Vector3 currPos;
 	
 	float initialTime = 0;
 	
 	void Start () {
         gameObject.renderer.material.color = RandomLogic.GetColor();
-		currPos = Camera.main.WorldToScreenPoint(transform.position);
 	}
 	
 	void Update () {
@@ -47,10 +48,27 @@ public class SphereActions : MonoBehaviour {
 
 			initialTime = 0;
 		};
+
+        doMove();
 	}
 
+    void doMove()
+    {
+        if (Vector3.Distance(transform.position, targetPos) < 7.0f)
+        {
+            targetPos = GameLogic.Instance.getRandomSpacePoint();
+            targetPos = new Vector3(targetPos.x, targetPos.y, transform.position.z);
+        }
+        else
+        {
+            Vector3 newPos = Vector3.Slerp(transform.position, targetPos, Time.deltaTime / 2);
+            transform.position = new Vector3(newPos.x, newPos.y, transform.position.z);
+        }
+
+    }
     void OnGUI()
     {
+        currPos = Camera.main.WorldToScreenPoint(transform.position);
         AdvancedLabel.Draw(new Rect(currPos.x + offsetx, Screen.height - currPos.y + offsety, 100, 100), letter, new NewFontSize(28), new NewColor(Color.black), new NewFontStyle(FontStyle.Bold));
         AdvancedLabel.Draw(new Rect(currPos.x + offsetx, Screen.height - currPos.y + offsety, 100, 100), letter, new NewFontSize(25));
     }
